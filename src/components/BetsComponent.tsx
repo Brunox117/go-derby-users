e24ca54ef7b7b2ee454486d9ac6f91b6dde67d76
Bruno Jimenez <omarjm0910@gmail.com>
2025-10-14 23:04:30 -0600

    agregamos un borde a las apuestas

--- src/components/BetsComponent.tsx
+++ src/components/BetsComponent.tsx
@@ -6,17 +6,17 @@ export const BetsComponent = () => {
   return (
     <Card>
       <CardHeader>
-        <CardTitle className="text-lg font-bold text-center">
+        <CardTitle className="text-3xl font-bold text-center">
           Gallo 1 vs Gallo 2
         </CardTitle>
         <CardContent>
-          <div className="flex justify-center flex-col items-center">
+          <div className="mt-4 flex justify-center flex-col items-center">
             <h2 className="text-lg font-bold text-center bg-green-700 text-white rounded-md p-2 max-w-md">
               Apuestas abiertas
             </h2>
           </div>
           <div className="grid grid-cols-2 gap-4 flex-col justify-between mt-5">
-            <div className="grid justify-center items-center">
+            <div className="grid justify-center items-center border rounded-md p-4">
               <p className="mb-4 text-md font-bold text-center bg-green-700 text-white rounded-md p-2 max-w-md">
                 Apuestas al verde
               </p>
@@ -26,8 +26,8 @@ export const BetsComponent = () => {
                 <Button>Tomo 80</Button>
               </div>
             </div>
-            <div className="grid justify-center items-center">
-              <p className="text-md font-bold text-center bg-red-700 text-white rounded-md p-2 max-w-md">
+            <div className="grid justify-center items-center border rounded-md p-4">
+              <p className="mb-4 text-md font-bold text-center bg-red-700 text-white rounded-md p-2 max-w-md">
                 Apuestas al rojo
               </p>
               <div className="grid  gap-2">
